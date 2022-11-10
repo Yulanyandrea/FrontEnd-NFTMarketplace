ede8faa17ec4aa9023da99f521f8ae85c20bd5bb
Yulany Munevar <andra_852@hotmail.com>
2022-11-10 10:07:24 -0500

    components

---
+++ src/components/ourCollection/OurCollection.jsx
@@ -0,0 +1,15 @@
+function OurCollection(){
+    return(
+        <div className="cardOurCollection__container">
+            <header className="cardOurCollection__menu">
+                <h2 className="cardOurCollection__title">Our collection</h2>
+            </header>
+
+            <footer className="cardOurCollection__pages">
+                <button className="cardOurCollection__previous">Previous</button>
+                <button className="cardOurCollection__next">Next</button>
+            </footer>
+
+        </div>
+    )
+}
---
+++ src/components/topSellers/TopSeller.jsx
@@ -0,0 +1,23 @@
+function TopSeller(){
+    return(
+        <div className="cardTopCollection__container">
+            <img src="" alt="" className="cardTopCollection__image" />
+            <img src="" alt="" className="cardTopCollection__imageProfile" />
+            <section className="cardTopCollection__imageSection">
+                <img src="" alt="" />
+                <img src="" alt="" />
+                <img src="" alt="" />
+            </section>
+
+            <section  className="cardTopCollection__detail">
+                <h2 className="cardTopCollection__titleNFT">Cubic Trad</h2>
+                <button className="cardTopCollection__items"></button>
+
+            </section>
+
+
+        </div>
+    )
+}
+
+export default TopSeller
