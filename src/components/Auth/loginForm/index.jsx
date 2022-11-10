17ff82650449d5ee2f457bdc19f8ca21147ce199
CP <cpimentel.0325@gmail.com>
2022-11-10 11:15:04 -0500

    Update proyect

---
+++ src/components/Auth/loginForm/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const LoginForm = () => (
+  <div>
+    <h1>Estamos en el login...</h1>
+  </div>
+);
+
+export default LoginForm;
--- src/index.jsx
+++ src/index.jsx
@@ -1,14 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import LoginForm from './components/Auth/loginForm';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+  },
+  {
+    path: '/login',
+    element: <LoginForm />,
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
