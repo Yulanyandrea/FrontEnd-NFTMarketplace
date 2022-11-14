206544ac1600763ce04adb6d0ca9c2496be18e79
Yulany Munevar <andra_852@hotmail.com>
2022-11-14 15:27:52 -0500

    footer/topCollection/TopSeller

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ const App = () => (
   <div className="App">
     {/* <TopCollection imageSell={tree} imageProfile={profile} /> */}
     {/* <Footer image={happy} name="picture" characteristic="Highest bid 1/20" price={30} /> */}
-    <TopSeller imageTop={flower} textTop="TrodBrand" priceTop="$400" />
+    {/* <TopSeller imageTop={flower} textTop="TrodBrand" priceTop="$400" /> */}
   </div>
 );
 
