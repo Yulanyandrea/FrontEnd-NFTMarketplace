4a8ed075895632edc4ce1d411673c0aa2864d8c4
Juan <juansmuar@gmail.com>
2022-11-15 14:02:13 -0500

    First version Navbar Component

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ const App = () => (
     {/* <Footer image={happy} name="picture" characteristic="Highest bid 1/20" price={30} /> */}
     {/* <TopSeller imageTop={flower} textTop="TrodBrand" priceTop="$400" /> */}
     {/* <NftCard bit="5+" name="NameStroam" bid="1/20" price="0.244" likes="532" /> */}
+    {/* <Navbar /> */}
   </div>
 );
 
