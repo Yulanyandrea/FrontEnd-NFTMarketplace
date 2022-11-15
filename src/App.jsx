5673b42ea233b47815a0c996bdb434eb1c780050
Juan <juansmuar@gmail.com>
2022-11-14 22:27:21 -0500

    Nft card componente

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import './App.css';
-import NftCard from './components/NftCard';
 
 const App = () => (
   <div>
     {/* <TopCollection imageSell={tree} imageProfile={profile} /> */}
     {/* <Footer image={happy} name="picture" characteristic="Highest bid 1/20" price={30} /> */}
     {/* <TopSeller imageTop={flower} textTop="TrodBrand" priceTop="$400" /> */}
-    <NftCard bit="5+" name="NameStroam" bid="1/20" price="0.244" likes="532" />
+    {/* <NftCard bit="5+" name="NameStroam" bid="1/20" price="0.244" likes="532" /> */}
   </div>
 );
 
