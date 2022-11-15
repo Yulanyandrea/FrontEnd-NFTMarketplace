4bbea1e1bf580ad02ab4c860c44c820882abea75
juansmuar <38388872+juansmuar@users.noreply.github.com>
2022-11-15 10:41:58 -0500

    Feature/nmp 40 nft card (#7)
    * Footer, topseller, top/ourCollection (#6)
    
    * footer/topCollection/TopSeller
    
    * footer/topCollection/TopSeller
    
    * footer/topCollection/TopSeller
    
    * Nft card componente
    
    Co-authored-by: Yulany  Andrea Munévar <79812118+Yulanyandrea@users.noreply.github.com>

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ const App = () => (
     {/* <TopCollection imageSell={tree} imageProfile={profile} /> */}
     {/* <Footer image={happy} name="picture" characteristic="Highest bid 1/20" price={30} /> */}
     {/* <TopSeller imageTop={flower} textTop="TrodBrand" priceTop="$400" /> */}
+    {/* <NftCard bit="5+" name="NameStroam" bid="1/20" price="0.244" likes="532" /> */}
   </div>
 );
 
