f61b415e533d0e153a83c31f51a09abc5d6e7357
Yulany  Andrea Munévar <79812118+Yulanyandrea@users.noreply.github.com>
2022-11-15 20:36:01 -0500

    Nmp38footer (#10)
    * footer/topCollection/TopSeller
    
    * footer/topCollection/TopSeller
    
    * footer/topCollection/TopSeller
    
    * fixing Top/Footer/Seller

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,12 @@
+// eslint-disable-next-line import/no-unresolved
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
+// eslint-disable-next-line no-undef
 test('renders learn react link', () => {
+  // eslint-disable-next-line react/jsx-filename-extension
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
+  // eslint-disable-next-line no-undef
   expect(linkElement).toBeInTheDocument();
 });
