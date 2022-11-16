a4ffd9ea38cc22f95571badddc835cc1f40ec45b
Juan <juansmuar@gmail.com>
2022-11-15 20:31:05 -0500

    small changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
 import './App.css';
+
 import CreateNFT from './components/CreateNft';
-// import Footer from './components/Footer/index';
-// import Navbar from './components/Navbar/index';
+// import Footer from './components/footer/index';
+import Navbar from './components/Navbar/index';
 
 const App = () => (
   <div className="App">
-    {/* <Navbar /> */}
-    <CreateNFT />
     {/* <TopCollection imageSell={tree} imageProfile={profile} /> */}
-    {/* <Footer image={happy} name="picture" characteristic="Highest bid 1/20" price={30} /> */}
     {/* <TopSeller imageTop={flower} textTop="TrodBrand" priceTop="$400" /> */}
     {/* <NftCard bit="5+" name="NameStroam" bid="1/20" price="0.244" likes="532" /> */}
+    {/* <Footer image={happy} name="picture" characteristic="Highest bid 1/20" price={30} /> */}
+    <Navbar />
+    <CreateNFT />
   </div>
 );
 
--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,4 +1,5 @@
 import './styles.scss';
+import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 // eslint-disable-next-line object-curly-newline
 import { faMagnifyingGlass, faBars, faSun, faBell } from '@fortawesome/free-solid-svg-icons';
@@ -8,49 +9,47 @@ import logo from '../../assets/logo.png';
 const Navbar = () => {
   return (
     <header className="navbar">
-      <a href="http://localhost:3000/" className="logo__link">
+      <Link to="/" className="logo__link">
         <img src={logo} alt="Nuron-logo" className="navbar__logo" />
-      </a>
-      <div className="menu__container">
-        <nav className="menu">
-          <ul className="menu__list">
-            <li className="menu__item">
-              <a href="http://localhost:3000/" className="menu__link">Home</a>
-            </li>
-            <li className="menu__item">
-              <a href="http://localhost:3000/" className="menu__link">About</a>
-            </li>
-            <li className="menu__item">
-              <a href="http://localhost:3000/" className="menu__link">Explore</a>
-            </li>
-            <li className="menu__item">
-              <a href="http://localhost:3000/" className="menu__link">Pages</a>
-            </li>
-            <li className="menu__item">
-              <a href="http://localhost:3000/" className="menu__link">Blog</a>
-            </li>
-            <li className="menu__item">
-              <a href="http://localhost:3000/" className="menu__link">Contact</a>
-            </li>
-          </ul>
-        </nav>
-      </div>
+      </Link>
+      <nav className="menu">
+        <ul className="menu__list">
+          <li className="menu__item">
+            <Link to="/" className="menu__link">Home</Link>
+          </li>
+          <li className="menu__item">
+            <Link to="/login" className="menu__link">Login</Link>
+          </li>
+          <li className="menu__item">
+            <Link to="/register" className="menu__link">Register</Link>
+          </li>
+          <li className="menu__item">
+            <Link to="/createnft" className="menu__link">Create</Link>
+          </li>
+          <li className="menu__item">
+            <Link to="/" className="menu__link">Blog</Link>
+          </li>
+          <li className="menu__item">
+            <Link to="/" className="menu__link">Contact</Link>
+          </li>
+        </ul>
+      </nav>
       <div className="buttons__container">
         <form className="menu__form">
-          <input className="menu__input" />
-          <button type="button" className="menu__button">
-            <FontAwesomeIcon icon={faMagnifyingGlass} />
+          <input type="text" className="menu__input" />
+          <button type="button" className="menu__button button__search">
+            <FontAwesomeIcon icon={faMagnifyingGlass} style={{ color: '#ACACAC' }} />
           </button>
         </form>
         <button type="button" className="menu__button--color">Wallet connect</button>
         <button type="button" className="menu__button">
-          <FontAwesomeIcon icon={faBell} />
+          <FontAwesomeIcon icon={faBell} style={{ color: '#ACACAC' }} />
         </button>
         <button type="button" className="menu__button button__bars">
-          <FontAwesomeIcon icon={faBars} />
+          <FontAwesomeIcon icon={faBars} style={{ color: '#ACACAC' }} />
         </button>
         <button type="button" className="menu__button">
-          <FontAwesomeIcon icon={faSun} />
+          <FontAwesomeIcon icon={faSun} style={{ color: '#ACACAC' }} />
         </button>
       </div>
     </header>
