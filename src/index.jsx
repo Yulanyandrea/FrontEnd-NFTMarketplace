9300a470e023fad093606c9f284fc6f32978fbc4
juansmuar <38388872+juansmuar@users.noreply.github.com>
2022-11-17 14:40:31 -0500

    routing in App (#17)

--- src/index.jsx
+++ src/index.jsx
@@ -1,36 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 // import './index.scss';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+// import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import LoginForm from './components/Auth/loginForm';
-import RegisterForm from './components/Auth/RegisterForm';
-import CreateNFT from './components/CreateNft';
+// import LoginForm from './components/Auth/loginForm';
+// import RegisterForm from './components/Auth/RegisterForm';
+// import CreateNFT from './components/CreateNft';
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />,
-  },
-  {
-    path: '/login',
-    element: <LoginForm />,
-  },
-  {
-    path: '/register',
-    element: <RegisterForm />,
-  },
-  {
-    path: '/createnft',
-    element: <CreateNFT />,
-  },
-]);
+// const router = createBrowserRouter([
+//   {
+//     path: '/',
+//     element: <App />,
+//   },
+//   {
+//     path: '/login',
+//     element: <LoginForm />,
+//   },
+//   {
+//     path: '/register',
+//     element: <RegisterForm />,
+//   },
+//   {
+//     path: '/createnft',
+//     element: <CreateNFT />,
+//   },
+// ]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    {/* <RouterProvider router={router} /> */}
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
 );
 
