fa00908780cea88553825ec46d9bdaf35a33fe9b
Yulany Munevar <andra_852@hotmail.com>
2022-11-18 21:34:53 -0500

    component create

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,39 @@
 import './App.css';
+// import { Routes, Route } from 'react-router-dom';
 
-import CreateNFT from './components/CreateNft';
-// import Footer from './components/Footer/index';
 // import Navbar from './components/Navbar/index';
+// import LoginForm from './components/Auth/loginForm';
+// import RegisterForm from './components/Auth/RegisterForm';
+// import CreateNFT from './components/CreateNft';
+// import Footer from './components/Footer/index';
+// import happy from './components/Footer/images/happy.jpg';
+import Create from './components/CreateandSell';
+import message from './components/CreateandSell/images/message.jpg';
 
 const App = () => (
   <div className="App">
+    <Create
+      title="STEP-01"
+      imageHeader={message}
+      titleArticle="Set up your wallet"
+      description="Powerful features and inclusions,
+    which makes Nuron standout, easily customizable and scalable."
+    />
     {/* <Navbar /> */}
-    <CreateNFT />
+    {/* <CreateNFT /> */}
     {/* <TopCollection imageSell={tree} imageProfile={profile} /> */}
     {/* <TopSeller imageTop={flower} textTop="TrodBrand" priceTop="$400" /> */}
     {/* <NftCard bit="5+" name="NameStroam" bid="1/20" price="0.244" likes="532" /> */}
-    {/* <Footer image={happy} name="picture" characteristic="Highest bid 1/20" price={30} /> */}
     {/* <Navbar />
-    <CreateNFT /> */}
+
+    <Routes>
+      <Route path="/login" element={<LoginForm />} />
+      <Route path="/register" element={<RegisterForm />} />
+      <Route path="/createnft" element={<CreateNFT />} />
+    </Routes> */}
+
+    {/* <Footer image={happy} name="picture" characteristic="Highest bid 1/20" price={30} /> */}
+
   </div>
 );
 
