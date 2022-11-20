c9ce88e3252392fbbbcb1db466cefd775884ef26
Juan <juansmuar@gmail.com>
2022-11-20 14:19:03 -0500

    crlf por lf en algunos archivos

--- src/App.jsx
+++ src/App.jsx
@@ -7,24 +7,24 @@ import RegisterForm from './components/Auth/RegisterForm';
 import CreateNFT from './components/CreateNft';
 import Footer from './components/Footer/index';
 import happy from './components/Footer/images/happy.jpg';
-// import Create from './components/CreateandSell';
-// import message from './components/CreateandSell/images/message.jpg';
-
+import Create from './components/CreateandSell';
+import message from './components/CreateandSell/images/message.jpg';
+import NftCard from './components/NftCard';
 
 const App = () => (
   <div className="App">
-    {/* <Create
+    <Create
       title="STEP-01"
       imageHeader={message}
       titleArticle="Set up your wallet"
       description="Powerful features and inclusions,
     which makes Nuron standout, easily customizable and scalable."
-    /> */}
+    />
     {/* <Navbar /> */}
     {/* <CreateNFT /> */}
     {/* <TopCollection imageSell={tree} imageProfile={profile} /> */}
     {/* <TopSeller imageTop={flower} textTop="TrodBrand" priceTop="$400" /> */}
-    {/* <NftCard bit="5+" name="NameStroam" bid="1/20" price="0.244" likes="532" /> */}
+    <NftCard bit="5+" name="NameStroam" bid="1/20" price="0.244" likes="532" />
     <Navbar />
 
     <Routes>
