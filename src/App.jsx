6e85852d54db80eebc1ddc23ac6993bf31919a57
juansmuar <38388872+juansmuar@users.noreply.github.com>
2022-11-21 11:33:10 -0500

    Feature/nmp 43 product detail (#22)
    * crlf por lf en algunos archivos
    
    * product details primer maquetacion
    
    * detalle de producto con datos en json
    
    * small changes
    
    * card dinamica
    
    * cambio json

--- src/App.jsx
+++ src/App.jsx
@@ -11,32 +11,35 @@ import happy from './components/Footer/images/happy.jpg';
 // import Create from './components/CreateandSell';
 // import message from './components/CreateandSell/images/message.jpg';
 
-const App = () => (
-  <div className="App">
-    {/* <Create
-      title="STEP-01"
-      imageHeader={message}
-      titleArticle="Set up your wallet"
-      description="Powerful features and inclusions,
-    which makes Nuron standout, easily customizable and scalable."
-    /> */}
-    {/* <Navbar /> */}
-    {/* <CreateNFT /> */}
-    {/* <TopCollection imageSell={tree} imageProfile={profile} /> */}
-    {/* <TopSeller imageTop={flower} textTop="TrodBrand" priceTop="$400" /> */}
-    {/* <NftCard bit="5+" name="NameStroam" bid="1/20" price="0.244" likes="532" /> */}
-    <Navbar />
+import ProductDetails from './components/ProductDetails';
 
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/login" element={<LoginForm />} />
-      <Route path="/register" element={<RegisterForm />} />
-      <Route path="/createnft" element={<CreateNFT />} />
-    </Routes>
+const App = () => {
+  return (
+    <div className="App">
+      {/* <Create
+        title="STEP-01"
+        imageHeader={message}
+        titleArticle="Set up your wallet"
+        description="Powerful features and inclusions,
+      which makes Nuron standout, easily customizable and scalable."
+      /> */}
+      {/* <CreateNFT /> */}
+      {/* <TopCollection imageSell={tree} imageProfile={profile} /> */}
+      {/* <TopSeller imageTop={flower} textTop="TrodBrand" priceTop="$400" /> */}
+      {/* <NftCard bit="5+" name="NameStroam" bid="1/20" price="0.244" likes="532" /> */}
+      <Navbar />
 
-    <Footer image={happy} name="picture" characteristic="Highest bid 1/20" price={30} />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/productdetail" element={<ProductDetails bid="0.244" number="22" owner="Broodband" />} />
+        <Route path="/login" element={<LoginForm />} />
+        <Route path="/register" element={<RegisterForm />} />
+        <Route path="/createnft" element={<CreateNFT />} />
+      </Routes>
 
-  </div>
-);
+      <Footer image={happy} name="picture" characteristic="Highest bid 1/20" price={30} />
+    </div>
+  );
+};
 
 export default App;
---
+++ src/components/ProductDetails/index.jsx
@@ -0,0 +1,62 @@
+import './styles.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHeart as farHeart } from '@fortawesome/free-regular-svg-icons';
+import image from '../../assets/image-equilibrium.jpg';
+import avatar from '../../assets/image-avatar.png';
+
+// eslint-disable-next-line react/prop-types, object-curly-newline
+const ProductDetails = ({ name, likes, bid, number, owner }) => {
+  return (
+    <div className="product__container">
+      <section className="images__container">
+        <img src={image} alt="NFT" className="product__image" />
+      </section>
+      <section className="info__container">
+        <section className="product__info">
+          <h1 className="product__title--color">{name}</h1>
+          <div className="title__buttons">
+            <div className="product__like">
+              <button type="button" className="like__button">
+                <FontAwesomeIcon icon={farHeart} />
+              </button>
+              <h4 className="product__totalikes">{likes}</h4>
+            </div>
+            <button className="product__button" type="button">...</button>
+          </div>
+        </section>
+        <section>
+          <h4 className="product__bid">Height bid <span className="product__bid--color">{bid}wETH</span></h4>
+          <h2 className="product__ranking">#{number} Portal, Info Bellow</h2>
+        </section>
+        <section className="info__buttons">
+          <button className="info__button" type="button">Bids</button>
+          <button className="info__button" type="button">Details</button>
+          <button className="info__button" type="button">History</button>
+        </section>
+        <section className="product__owner">
+          <h4>Owner</h4>
+          <img src={avatar} alt="avatar" className="owner__avatar" />
+          <h3>{owner}</h3>
+        </section>
+        <section>
+          <h4>Property</h4>
+          <div className="product__tag">
+            <h4>HYPE TYPE</h4>
+            <h3>CALM AF (STILL)</h3>
+          </div>
+        </section>
+        <section className="place__bid">
+          <h4>Wining bit</h4>
+          <div className="bid__owner">
+            <img src={avatar} alt="avatar" className="owner__avatar" />
+            <h3>Heighest bid {owner}</h3>
+            <h3>{bid}wETH</h3>
+          </div>
+          <button className="bid__button" type="button">Place a Bid</button>
+        </section>
+      </section>
+    </div>
+  );
+};
+
+export default ProductDetails;
