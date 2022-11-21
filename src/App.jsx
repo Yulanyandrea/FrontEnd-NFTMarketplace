a0ba4843c91fed23c21eb57bdfb228586d16e503
Juan <juansmuar@gmail.com>
2022-11-21 08:43:18 -0500

    detalle de producto con datos en json

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,47 @@
 import './App.scss';
 import { Routes, Route } from 'react-router-dom';
 
-// import Navbar from './components/Navbar/index';
+import products from './assets/products.json';
+
+import Navbar from './components/Navbar/index';
 import LoginForm from './components/Auth/loginForm';
 import RegisterForm from './components/Auth/RegisterForm';
 import CreateNFT from './components/CreateNft';
 import Footer from './components/Footer/index';
 import Home from './components/Home';
 import happy from './components/Footer/images/happy.jpg';
-import Create from './components/CreateandSell';
-import message from './components/CreateandSell/images/message.jpg';
-import NftCard from './components/NftCard';
+// import Create from './components/CreateandSell';
+// import message from './components/CreateandSell/images/message.jpg';
+import ListNft from './components/ListNft';
+import ProductDetails from './components/ProductDetails';
 
-const App = () => (
-  <div className="App">
-    <Create
-      title="STEP-01"
-      imageHeader={message}
-      titleArticle="Set up your wallet"
-      description="Powerful features and inclusions,
-    which makes Nuron standout, easily customizable and scalable."
-    />
-    {/* <Navbar /> */}
-    {/* <CreateNFT /> */}
-    {/* <TopCollection imageSell={tree} imageProfile={profile} /> */}
-    {/* <TopSeller imageTop={flower} textTop="TrodBrand" priceTop="$400" /> */}
-    <NftCard bit="5+" name="NameStroam" bid="1/20" price="0.244" likes="532" />
-    <Navbar />
+const App = () => {
+  return (
+    <div className="App">
+      {/* <Create
+        title="STEP-01"
+        imageHeader={message}
+        titleArticle="Set up your wallet"
+        description="Powerful features and inclusions,
+      which makes Nuron standout, easily customizable and scalable."
+      /> */}
+      {/* <CreateNFT /> */}
+      {/* <TopCollection imageSell={tree} imageProfile={profile} /> */}
+      {/* <TopSeller imageTop={flower} textTop="TrodBrand" priceTop="$400" /> */}
+      {/* <NftCard bit="5+" name="NameStroam" bid="1/20" price="0.244" likes="532" /> */}
+      <Navbar />
 
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/login" element={<LoginForm />} />
-      <Route path="/register" element={<RegisterForm />} />
-      <Route path="/createnft" element={<CreateNFT />} />
-    </Routes>
+      <Routes>
+        <Route path="/" element={<ListNft products={products} />} />
+        <Route path="/productdetail" element={<Home bid="0.244" number="22" owner="Broodband" />} />
+        <Route path="/login" element={<LoginForm />} />
+        <Route path="/register" element={<RegisterForm />} />
+        <Route path="/createnft" element={<CreateNFT />} />
+      </Routes>
 
-    {/* <Footer image={happy} name="picture" characteristic="Highest bid 1/20" price={30} /> */}
-
-  </div>
-);
+      <Footer image={happy} name="picture" characteristic="Highest bid 1/20" price={30} />
+    </div>
+  );
+};
 
 export default App;
