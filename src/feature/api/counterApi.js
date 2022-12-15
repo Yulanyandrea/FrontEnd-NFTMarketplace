2fb4ef92af69a89aa98a5c3c02f7127cb561f20c
CP <cpimentel.0325@gmail.com>
2022-12-15 12:25:03 -0500

    Update component Create NFT and fix Redux

--- src/feature/api/counterApi.js
+++ src/feature/api/counterApi.js
@@ -17,7 +17,6 @@ export async function GetData(data) {
     return data;
   } catch (error) {
     // eslint-disable-next-line
-<<<<<<< HEAD
     console.error(error);
   }
 }
@@ -38,8 +37,6 @@ export async function FetchPost(data) {
     return data;
   } catch (error) {
     // eslint-disable-next-line
-=======
->>>>>>> af67316a5da8ddde78eb6776db81efdafee97a57
     console.error(error);
   }
 }
